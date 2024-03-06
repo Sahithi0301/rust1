4bd141e8802eb139801d7173e522291a4c7dbbc6
Sahithi0301 <sahithiarekatla@gmail.com>
2024-03-06 15:47:27 -0500

    First Commit

---
+++ target/debug/deps/sample2-32d404209876f143.d
@@ -0,0 +1,5 @@
+/Users/sahithiarekatla/sample2/target/debug/deps/libsample2-32d404209876f143.rmeta: src/main.rs
+
+/Users/sahithiarekatla/sample2/target/debug/deps/sample2-32d404209876f143.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/sample2-588771a13f643ff6.d
@@ -0,0 +1,5 @@
+/Users/sahithiarekatla/sample2/target/debug/deps/libsample2-588771a13f643ff6.rmeta: src/main.rs
+
+/Users/sahithiarekatla/sample2/target/debug/deps/sample2-588771a13f643ff6.d: src/main.rs
+
+src/main.rs:
